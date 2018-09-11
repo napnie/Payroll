836003d307a2129d239b4042b10b101167120c3e
masty123 <theeruth.b@ku.th>
2018-09-11 13:44:26 +0700

    edited Employee.java

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,49 +1,76 @@
 package model;
 
-import transaction.PaymentSchedule;
-import transaction.PaymentClassification;
-import transaction.PaymentMethod;
+import transaction.*;
+
+
 
 public class Employee {
 
 	private PaymentSchedule schedule;
 	private PaymentClassification classification;
-	private int id;
+	private PaymentMethod method;
+	private Affiliation affiliation;
+	private final int empid;
+	private String name;
+	private String address;
+	
+	
+	public Employee(int empid, String name, String address) {
+		this.empid = empid;
+		this.name = name;
+		this.address = address;
+	}
+	
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public int getId() {
+		return this.empid;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 	
 	public void setSchedule(PaymentSchedule schedule) {
 		this.schedule = schedule;
 	}
 
+	public PaymentSchedule getSchedule() {
+		return this.schedule;
+	}
+	
 	public void setClassification(PaymentClassification classification) {
 		this.classification = classification;
 	}
-	
-	public int getId() {
-		return this.id;
-	}
-
-	public Object getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public double getSalary() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public PaymentSchedule getSchedule() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	public PaymentClassification getClassification() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.classification;
+	}
+	
+	public void setAffiliation(Affiliation affiliation) {
+		this.affiliation = affiliation;
+	}
+	
+	public Affiliation getAffiliation() {
+		return this.affiliation;
+	}
+	
+	public void setMethod(PaymentMethod method) {
+		this.method = method;
 	}
 
 	public PaymentMethod getMethod() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.method;
 	}
 }
